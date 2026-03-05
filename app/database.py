a28decfd80cda7e42aeed50dabd581758837d4ab
Naman Vora <naman.vora@ispl.com>
2026-03-05 18:47:23 +0530

    remove localhost fallback

--- app/database.py
+++ app/database.py
@@ -1,11 +1,30 @@
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker, declarative_base
+
+# DATABASE_URL = "postgresql://postgres:newpass123@localhost:5432/finance_ai"
+
+# engine = create_engine(DATABASE_URL)
+
+# SessionLocal = sessionmaker(
+#     autocommit=False,
+#     autoflush=False,
+#     bind=engine
+# )
+
+# Base = declarative_base()
+
+# def get_db():
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql://postgres:newpass123@localhost:5432/finance_ai"
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     DATABASE_URL,
